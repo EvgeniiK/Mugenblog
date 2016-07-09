35ebb8d3cd3db514c542cafc1d32e517f78db5a9
EvgeniiK <evgenkuznetso@gmail.com>
2016-07-09 13:39:54 +0300

    set deptoy settings

---
+++ config/deploy.rb
@@ -0,0 +1,19 @@
+# config valid only for current version of Capistrano
+lock '3.5.0'
+
+set :repo_url, 'git@github.com:EvgeniiK/Mugenblog.git'
+
+set :use_sudo, false
+set :bundle_binstubs, nil
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache',
+                                               'tmp/sockets', 'vendor/bundle',
+                                               'public/system')
+
+after 'deploy:publishing', 'deploy:restart'
+
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:reload'
+  end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,24 @@
+set :port, 22
+set :user, 'mugen'
+set :deploy_via, :remote_cache
+set :use_sudo, false
+set :rvm_ruby_version, '2.3.1'
+set :branch, 'master'
+set :application, 'mugenblog'
+
+server '46.101.211.90',
+  roles: [:web, :app, :db],
+  port: fetch(:port),
+  user: fetch(:user),
+  primary: true
+
+set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
+
+set :ssh_options, {
+  forward_agent: true,
+  auth_methods: %w(publickey),
+  user: 'mugen',
+}
+
+set :rails_env, :production
+set :conditionally_migrate, true
