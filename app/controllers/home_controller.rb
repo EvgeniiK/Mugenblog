236adca392b9c2fa39b7f32f3c55779123f11f5b
EvgeniiK <lordlenoks@gmail.com>
2017-03-27 20:46:24 +0300

    add pagination to articles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.all
+    @articles = Article.order(:created_at).page(params[:page]).per(5)
     @hints = Hint.all
   end
 end
