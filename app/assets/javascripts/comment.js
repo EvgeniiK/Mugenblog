12890793d25b523d2a4ed3fd20d07b6555b1d44f
EvgeniiK <lordlenoks@gmail.com>
2017-04-04 13:35:18 +0300

    change commets js, add animation, fix multiple triggering

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -2,8 +2,6 @@ $(document).ready(function() {
 
     checkPresenceOfText('create');
     create();
-    destroy();
-    edit();
 
     function checkPresenceOfText(selector){
       $('trix-editor.comment-' + selector).on('trix-change', function(event) {
@@ -15,11 +13,12 @@ $(document).ready(function() {
       });
     };
 
+    // This function will sanitize text in editor to find at least one letter
     function strip(html){
       var tmp = document.createElement("DIV");
       tmp.innerHTML = html.replace(/&nbsp;/g,"").replace(/ /g,"");
       return tmp.textContent || tmp.innerText;
-    }
+    };
 
     function create(){
       $('.create-comment').click(function() {
@@ -28,37 +27,39 @@ $(document).ready(function() {
           url: $(this).data('url'),
           data: $('form').serializeArray()
         }).success(function(comment){
-          $('#comments').prepend(comment);
+          $(comment).hide().prependTo($('#comments')).animate({
+            easing: "easeInQuad",
+            height: "toggle"
+          }, 500);
           $('trix-editor').empty();
           $('#no-comments').remove();
-          edit();
-          destroy();
         });
       });
     };
 
-    function update(id){
+    function update(element, id){
       $('.update-comment').click(function() {
         $.ajax({
           type: "PATCH",
           url: $(this).data('url'),
           data: $('form').serializeArray()
         }).success(function(comment){
-          $('#' + id).replaceWith(comment);
-          edit();
+          element.fadeOut(300, function(){
+            var div = $(comment).hide();
+            element.replaceWith(div);
+            $('#' + id).fadeIn(300, function(){
+            });
+          });
         });
       });
     };
 
-    function edit(){
-      $('.edit-comment').click(function() {
-        var id = $(this).data('id');
-        $.ajax({
-          url: $(this).data('url'),
-        }).success(function(form){
-          $('#' + id).html(form);
-          update(id);
-          checkPresenceOfText('update');
+    function cancel(element, html) {
+      $('.cancel-update').click(function(){
+        element.fadeOut(300, function(){
+          element.html(html);
+          element.fadeIn(300, function(){
+          });
         });
       });
     };
@@ -69,17 +70,46 @@ $(document).ready(function() {
       }
     }
 
-    function destroy(){
-      $('.btn-delete').click(function(event) {
-          event.preventDefault();
-          var id = $(this).data('id');
-          $.ajax({
-              type: "DELETE",
-              url: $(this).attr('href'),
-          }).success(function(html){
-              $('#' + id).remove();
-              checkLastComment();
+    // These functions are global
+
+    // Edit button loads form partial instead comment
+    $(document).on('click', '.edit-comment', function() {
+        $('.cancel-update').click();
+        var id = $(this).data('id'),
+        element = $('#' + id),
+        html = element.html();
+
+        $.ajax({
+          url: $(this).data('url'),
+        }).success(function(form){
+          element.fadeOut(300, function(){
+            element.html(form);
+            element.fadeIn(300, function(){
+              update(element, id);
+              cancel(element, html);
+              checkPresenceOfText('update');
+            });
           });
+        });
       });
-    };
+
+    // Delete button removes comment
+    $(document).on('click', '.btn-delete', function(event) {
+      event.preventDefault();
+      var element = $('#' + $(this).data('id'));
+      $.ajax({
+          type: "DELETE",
+          url: $(this).attr('href'),
+      }).success(function(html){
+        element.animate({
+          easing: "easeOutQuad",
+          height: "toggle"
+        }, 300, function() {
+          element.remove();
+        });
+        // If this commet was the last, instead of him there will be message 'No comments yet'
+        checkLastComment();
+      });
+    });
 });
+
