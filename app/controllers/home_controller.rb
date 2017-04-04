d567527f3afa262451bf87e0c960d597c6403112
EvgeniiK <lordlenoks@gmail.com>
2017-04-04 15:55:04 +0300

    change articles order to desc

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @articles = Article.order(:created_at).page(params[:page]).per(5)
+    @articles = Article.order(created_at: :desc).page(params[:page]).per(5)
     @hints = Hint.all
   end
 end
